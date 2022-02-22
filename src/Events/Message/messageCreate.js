10052c85752f1407821112ba21c2dccc69a31024
Lillith3252 <74218901+Lilly3252@users.noreply.github.com>
2022-02-22 00:00:23 -0500

    Update messageCreate.js

--- src/Events/Message/messageCreate.js
+++ src/Events/Message/messageCreate.js
@@ -1,8 +1,5 @@
 const Event = require("../../Structures/Event");
-const config = require("../../config.json");;
-const { MessageEmbed } = require("discord.js");
 
-// Line 83 to 125 credit to ShinoTheShino
 
 module.exports = class extends Event {
   async run(message) {
